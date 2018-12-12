63dae650e0238f05d93ac1a7a4485145526459dc
Nick Stockton <sputnick1124@comcast.net>
2018-12-11 23:24:57 -0500

    First commit of initial schema and migrations

---
+++ migrations/2018-12-12-041242_create_participants/up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE participants (
+    id SERIAL PRIMARY KEY,
+    participants_id VARCHAR NOT NULL,
+    car_name VARCHAR,
+    weight FLOAT NOT NULL
+)
---
+++ src/schema.rs
@@ -0,0 +1,8 @@
+table! {
+    participants (id) {
+        id -> Int4,
+        participants_id -> Varchar,
+        car_name -> Nullable<Varchar>,
+        weight -> Float8,
+    }
+}
